79051d8a0ae6279114147dbe52e6faa10b2311c1
galmodovar <galmodovar1@gmail.com>
2021-12-15 10:49:39 -0600

    redeploy

--- trackit/settings.py
+++ trackit/settings.py
@@ -59,7 +59,11 @@ REST_FRAMEWORK = {
 # THIS IS NEW
 CORS_ORIGIN_WHITELIST = (
     'http://localhost:3000',
-    'http://127.0.0.1:3000'
+    'http://127.0.0.1:3000',
+    'https://trackit-client-side.herokuapp.com/',
+    'https://trackit-server-side.herokuapp.com/'
+    
+    
 )
 
 # UPDATE THIS
